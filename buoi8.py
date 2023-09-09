9ae6adc66df508a3d7fffcb9c1acd256506c469e
nvt13102008 <nvt13102008@gmail.com>
2023-09-09 20:13:47 +0700

    fix

---
+++ buoi8.py
@@ -0,0 +1,52 @@
+## Bai1.1
+# def is_even(num):
+#   return num % 2 == 0
+
+# num = int((input("Input a number: ")))
+# even = is_even(num)
+# if is_even(num):
+#   print("This number is even")
+# else:
+#   print("This number is not even")
+
+## Bai1.2
+# def cal_area(r):
+#   return r * r * 3.14
+
+# r = int(input("Input radius"))
+# area = cal_area(r)
+# print("Circle area:", area)
+
+##Bai1.3
+# def reverse_str(str):
+#   return str[::-1]
+  
+# str = input("Input a string: ")
+# reverse = reverse_str(str)
+# print(reverse)
+
+##Bai1.4
+# def is_palindrome(str):
+#   if str[::-1] != str:
+#     return False
+#   return True
+# str = input("Input a string: ")
+# palindrome = is_palindrome(str)
+# if palindrome == True:
+#   print("This is a palindrome")
+# else:
+#   print("This is not a palindrome")
+  
+##Bai2.1
+# def factorial(num):
+#   if num == 0:
+#     return 1
+#   elif num == 1:
+#     return 1
+#   elif num >= 2:
+#     return num * factorial(num - 1)
+
+# num = int(input("Input a number: "))
+# factorial_num = factorial(num)
+# print(factorial_num)
+
